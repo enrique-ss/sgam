fd8ce570a124607b30f09f65ed7d9faba60a3f5b
Enrique Silveira <105016903+enrique-ss@users.noreply.github.com>
2025-12-18 15:16:27 -0300

    clientes nao ve mais dash

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const PERMS = {
-    admin: { tabs: ['dash', 'clientes', 'demandas', 'entregas', 'config'], create: true, edit: true, manage: true, approve: true },
+    admin: { tabs: ['clientes', 'demandas', 'entregas', 'config'], create: true, edit: true, manage: true, approve: true },
     colaborador: { tabs: ['dash', 'demandas', 'entregas', 'config'], create: true, edit: false, manage: false, approve: true },
     cliente: { tabs: ['dash', 'demandas', 'entregas', 'config'], create: false, edit: false, manage: false, approve: false }
 };
